3ec5f3db681a722086d8d03c1d840200acad56af
Jason Chundusu <Jason.chundusu@gmail.com>
2026-08-13 00:26:33 +0100

    feat: Implement LLM insights

---
+++ core/ai_insights.py
@@ -0,0 +1,14 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+
+def prompt(self, text):
+    response = client.models.generate_content(
+        model='gemini-3.6-flash',
+        contents=text
+    )
+    return response.text
